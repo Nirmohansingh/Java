6808ce02cb6b5ca1ac27b71bd1cc0456480f2184
STAR <STAR@Lovi-PC>
2018-01-29 08:01:58 +0530

    first commit

---
+++ JAVA/src/New1.java
@@ -0,0 +1,150 @@
+// int to char
+
+
+public class New1
+{
+
+     public static void main(String []args){
+         int s = 65;
+         char a=(char)s;
+        System.out.println(a);
+     }
+}
+
+// long to string
+
+public class New1
+{
+
+     public static void main(String []args){
+        long f= 98687969809L;
+         String a =String.valueOf(f);
+
+        System.out.println(a);
+     }
+}
+
+
+//short to double
+
+public class New1
+{
+
+     public static void main(String []args){
+        short f= 10;
+      double a =f;
+        System.out.println(a);
+     }
+}
+
+
+// string to short
+
+public class New1
+{
+
+     public static void main(String []args){
+        String a= "235";
+      Short b = Short.valueOf(a);
+        System.out.println(b);
+     }
+}
+
+
+// byte to short
+
+public class New1
+{
+
+     public static void main(String []args){
+        byte a= 14;
+      short b = a;
+        System.out.println(b);
+     }
+}
+
+
+//boolean to string
+
+public class New1
+{
+
+     public static void main(String []args){
+       boolean x = true;
+       String y = String.valueOf(x); 
+        System.out.println(y);
+     }
+}
+
+//str to int
+
+public class Pk1
+{
+
+   public static void main(String []args){
+       String s ="345";
+       Integer a =Integer.valueOf(s);
+      System.out.println(+a);
+   }
+}
+
+char to int 
+public class Pk1
+{
+
+   public static void main(String []args){
+       char s ='n';
+       int a= s;
+      System.out.println(+a);
+   }
+}
+
+
+//double to int
+
+public class Pk1
+{
+
+   public static void main(String []args){
+       double f= 98.09d;
+       int a= (int)f;
+      System.out.println(+a);
+   }
+}
+
+//double to string
+
+public class Pk1
+{
+
+   public static void main(String []args){
+       double f= 98.09d;
+       String a =String.valueOf(f);
+
+      System.out.println(a);
+   }
+}
+
+//float to double
+
+public class Pk1
+{
+
+   public static void main(String []args){
+       float s =89.55f;
+       double a =s;
+      System.out.println(+a);
+   }
+}
+
+
+//string to float
+
+public class Pk1
+{
+
+   public static void main(String []args){
+       String s ="345";
+       Float a =Float.valueOf(s);
+      System.out.println(+a);
+   }
